4f6619e15ae02c25eea9f4c77251c6763e0f275e
Oyefule <drelanorgent@gmail.com>
2023-06-02 00:47:25 +0100

    Resolved folder structure

---
+++ client-side/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome!!! This is the frontend</h1>
+    </div>
+  );
+}
+
+export default App;
