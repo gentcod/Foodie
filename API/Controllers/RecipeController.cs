33fee5ec104f4d5e1d8f4f447739adc43222e9fe
Oyefule <drelanorgent@gmail.com>
2023-06-03 22:48:37 +0100

    SQLite database Initialization

--- API/Controllers/RecipeController.cs
+++ API/Controllers/RecipeController.cs
@@ -1,13 +1,20 @@
+using API.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
    public class RecipeController : BaseApiController
-    {
-        [HttpGet]
-        public  ActionResult GetRecipes()
-        {
-            return Ok();
-        }
-    }
+   {
+      private readonly RecipesContext _context;
+      public RecipeController(RecipesContext context)
+      {
+         _context = context;
+      }
+      [HttpGet]
+      public ActionResult GetRecipes()
+      {
+        var recipes = _context.Recipes;
+        return Ok(recipes);
+      }
+   }
 }
