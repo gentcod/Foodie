936b6b7ff7e0fcaffb15a2df63a6631e76b43581
Oyefule <drelanorgent@gmail.com>
2023-10-14 23:45:30 +0100

    User Auth init - Clerk initialization

--- client-side/src/index.tsx
+++ client-side/src/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ClerkProvider } from '@clerk/clerk-react'
+
 import 'leaflet/dist/leaflet.css'
 import "./index.css";
 import App from "./App";
@@ -8,16 +10,23 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 
+if (!process.env.REACT_APP_CLERK_PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
+const clerkPubKey = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <ClerkProvider publishableKey={clerkPubKey}>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </ClerkProvider>
   </React.StrictMode>
 );
 
